d3e0bf7e7687efcf9781f0442b9302b4cffa764e
OSpoon <zxin088@gmail.com>
2024-08-06 10:48:05 +0800

    refactor(vue-webpack-plugin): move VueLoaderPlugin instantiation to plugin loader
    Previously, the VueLoaderPlugin was directly instantiated within the apply method. This
    has been refactored to be part of the maybeUseVue asynchronous function to allow for
    potential asynchronous operations, such as installing dependencies, before using the
    plugin. This change decouples plugin instantiation from its usage and provides more
    flexibility in handling plugin dependencies.

--- vue-webpack-plugin.js
+++ vue-webpack-plugin.js
@@ -1,6 +1,12 @@
-module.exports = class VueWebpackPlugin {
-    async apply(compiler) {
-        compiler.options.module.rules = [
+/**
+ * This function must be asynchronous, as dependencies may be installed
+ * @returns 
+ */
+async function maybeUseVue() {
+
+    return {
+        plugins: [new (require('vue-loader').VueLoaderPlugin)],
+        loaders: [
             {
                 test: /\.vue$/,
                 loader: require.resolve('vue-loader'),
@@ -10,8 +16,41 @@ module.exports = class VueWebpackPlugin {
                 loader: require.resolve('babel-loader'),
                 exclude: /node_modules/
             }
-        ]
+        ],
+    }
+}
 
-        new (require('vue-loader').VueLoaderPlugin)().apply(compiler)
+module.exports = class VueWebpackPlugin {
+
+    /**
+     * before
+     * @param {*} compiler 
+     */
+    // async apply(compiler) {
+    //     const maybeInstallVue = await maybeUseVue()
+
+    //     compiler.options.module.rules = [
+    //         ...(maybeInstallVue?.loaders || []),
+    //         ...compiler.options.module.rules
+    //     ].filter(Boolean)
+
+    //     maybeInstallVue?.plugins?.forEach((plugin) => plugin.apply(compiler))
+    // }
+
+    /**
+     * after
+     * @param {*} compiler 
+     */
+    async apply(compiler) {
+        compiler.hooks.beforeRun.tapPromise('VueWebpackPlugin', async (compiler) => {
+            const maybeInstallVue = await maybeUseVue()
+
+            compiler.options.module.rules = [
+                ...(maybeInstallVue?.loaders || []),
+                ...compiler.options.module.rules
+            ].filter(Boolean)
+
+            maybeInstallVue?.plugins?.forEach((plugin) => plugin.apply(compiler))
+        });
     }
 }
